1f33076ae7288eafb45b6457083f34e9a67d9251
isabellahuang4 <ixhuang1996@gmail.com>
2020-04-01 11:41:39 -0600

    comparing two step game to base game

--- simplefly/testing_3_31/test.py
+++ simplefly/testing_3_31/test.py
@@ -1,4 +1,6 @@
 #test exponential v regular simscore -- longest common substring
+#second test: twostep game v regular game 
+#      two step game HAS different species flies comparing simscores normally***
 
 import sys
 import random as r
@@ -7,19 +9,22 @@ import csv
 from itertools import combinations
 from statistics import mean
 
-from commonsub_simscore import Firefly as subby_fly
 from exponential import Firefly as exp_fly
+from commonsub_simscore import Firefly as subby_fly
 
 LENGTH = 6
 MAX_FLASH = 4
 NUM_SPECIES = 3
 NUM_EACH = 15
-EPOCHS = 200 
+EPOCHS = 500 
 MUTATE_PROB = .1
-TRIALS = 15
+DECISION_THRESHOLD = .7
+PENALTY = -20
+REWARD = 10 
+TRIALS = 10
 
 #given list of fireflies, the epoch number, and whether we are nudging periodic
-def round(fireflies, epoch, push):
+def round_one(fireflies, epoch, push):
     for (i, j) in combinations(fireflies, 2):
         same = i.same_species(j)
         #same species                                                 
@@ -40,7 +45,7 @@ def round(fireflies, epoch, push):
             #when replicating, do so with chance of mutation                         
                 if i.simscore <= j.simscore:
                     j.pattern = i.pattern
-                    if r.random() < MUTATE_PROB and epoch < 195:
+                    if r.random() < MUTATE_PROB and epoch < 475:
                         if push and r.random() < .3:
                             j.push_periodic()
                         else:
@@ -48,12 +53,13 @@ def round(fireflies, epoch, push):
                     j.reset_simscore()
                 else:
                     i.pattern = j.pattern
-                    if r.random() < MUTATE_PROB and epoch < 195:
+                    if r.random() < MUTATE_PROB and epoch < 475:
                         if push and r.random() < .3:
                             i.push_periodic()
                         else:
                             i.mutate()
                     i.reset_simscore()
+
         #diff species                                                                    
         else:
             if i.pattern == None:
@@ -63,6 +69,39 @@ def round(fireflies, epoch, push):
             distance = i.calc_similarity(j)
             i.update_simscore(distance)
             j.update_simscore(distance)
+
+
+def round_two(fireflies):
+    r.shuffle(fireflies)
+    for (i,j) in combinations(fireflies, 2):
+        #i sends partial sequence
+        start = r.randint(0, LENGTH-4) #note we're picking indices
+        end = r.randint(start+3, LENGTH-1) #ensure that seq is at least 3 long
+        seq_length = end-start
+        seq = i.pattern[start:end]
+        #j makes decision based on substring match with j's full pattern
+        decision = j.make_decision(seq)
+        if decision < DECISION_THRESHOLD:
+            #decided different species
+            if i.same_species(j):
+                #neg penalty
+                i.update_score(PENALTY*(seq_length/LENGTH))
+                j.update_score(PENALTY*(seq_length/LENGTH))
+            else:
+                #pos reward 
+                i.update_score(REWARD*(LENGTH/seq_length))
+                j.update_score(REWARD*(LENGTH/seq_length))
+        else:
+            #decided same species
+            if i.same_species(j):
+                #pos reward
+                i.update_score(REWARD*(LENGTH/seq_length))
+                j.update_score(REWARD*(LENGTH/seq_length))
+            else:
+                #neg reward
+                i.update_score(PENALTY*(seq_length/LENGTH))
+                j.update_score(PENALTY*(seq_length/LENGTH))
+
 
 def list_flies(flies):
     flies.sort()
@@ -96,6 +135,35 @@ def print_csv(results):
 def main(args):
     runs = {}
 
+    #two step
+    print('two step')
+    for rep in range(TRIALS):
+        fireflies = [0] * (NUM_SPECIES * NUM_EACH)
+        for i in range(NUM_SPECIES):
+            for j in range(NUM_EACH):
+                fireflies[j+(NUM_EACH*i)] = subby_fly(i)
+        
+        for epoch in range(EPOCHS):
+            round_one(fireflies, epoch, True)
+            round_two(fireflies)
+        
+        runs[('twostep', rep)] = list_flies(fireflies)
+
+
+    #normal
+    print('normal')
+    for rep in range(TRIALS):
+        fireflies = [0] * (NUM_SPECIES * NUM_EACH)
+        for i in range(NUM_SPECIES):
+            for j in range(NUM_EACH):
+                fireflies[j+(NUM_EACH*i)] = subby_fly(i) 
+
+        for epoch in range(EPOCHS):
+            round_one(fireflies, epoch, True)
+
+        runs[('normal', rep)] = list_flies(fireflies)
+
+    '''
     #exponential
     print('exponential')
     for rep in range(TRIALS):
@@ -105,7 +173,7 @@ def main(args):
                 fireflies[j+(NUM_EACH*i)] = exp_fly(i)
 
         for epoch in range(EPOCHS):
-            round(fireflies, epoch, False)
+            round_one(fireflies, epoch, False)
 
         runs[('exp',rep)] = list_flies(fireflies)
 
@@ -118,7 +186,7 @@ def main(args):
                 fireflies[j+(NUM_EACH*i)] = subby_fly(i)
 
         for epoch in range(EPOCHS):
-            round(fireflies, epoch, False)
+            round_one(fireflies, epoch, False)
 
         runs[('normal', rep)] = list_flies(fireflies)
 
@@ -131,7 +199,7 @@ def main(args):
                 fireflies[j+(NUM_EACH*i)] = exp_fly(i)
 
         for epoch in range(EPOCHS):
-            round(fireflies, epoch, True)
+            round_one(fireflies, epoch, True)
 
         runs[('exp_push',rep)] = list_flies(fireflies)
 
@@ -144,11 +212,11 @@ def main(args):
                 fireflies[j+(NUM_EACH*i)] = subby_fly(i)
 
         for epoch in range(EPOCHS):
-            round(fireflies, epoch, True)
+            round_one(fireflies, epoch, True)
 
         runs[('normal_push', rep)] = list_flies(fireflies)
 
-
+    '''
     print('printing')
     print_csv(runs)
 
