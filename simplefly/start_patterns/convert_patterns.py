089a00ec157fe6b2791f7192be0496fea22490a9
isabellahuang4 <ixhuang1996@gmail.com>
2020-07-31 10:03:14 -0600

    recreate patterns from complexfly data points

--- simplefly/start_patterns/convert_patterns.py
+++ simplefly/start_patterns/convert_patterns.py
@@ -1,6 +1,8 @@
 import csv
 import pickle
 
+MAX_LEN = 140
+
 def read_pattern(line):
     p = []
     for i in range(len(line)):
@@ -17,3 +19,36 @@ def read_in(file_path):
     
     with open(file_path[:-4]+'.pickle', 'wb') as file:
         pickle.dump(p_list, file, protocol = pickle.HIGHEST_PROTOCOL)
+
+
+def convert_pattern(line):
+    num_flash = int(line[1])
+    flash_len = int(float(line[2])*10)
+    quiet = int(float(line[3])*10) - flash_len
+    #silence = int(float(line[4])*10) - (num_flash * (flash_len+quiet))
+
+    p = [0] * int(float(line[4])*10)
+    t = 0
+    for i in range(num_flash):
+        for j in range(flash_len):
+            p[t] = 1
+            t += 1
+        for k in range(quiet):
+            p[t] = 0
+            t += 1
+
+    return p
+            
+    
+def create_patterns(file_path):
+    p_list = []
+    with open(file_path) as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if row[0] != 'species':
+                p_list.append(convert_pattern(row))
+
+    with open(file_path[:-4]+'.pickle', 'wb') as file:
+        pickle.dump(p_list, file, protocol = pickle.HIGHEST_PROTOCOL)
+
+        
